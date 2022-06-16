e67cf35443a2d20713b74427f4db6908c8af880d
Pjotrs Baskakovs <baskakovs@me.com>
2022-06-16 21:37:04 +0300

    commit tree 39df521a950f20d05099422a57e647e17385a85b

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function countDown(integer){
+    let i = 0
+      while(i <= integer){
+          console.log(integer)
+          integer = integer - 1
+      }
+  }
+  const message = []
+  function writeCards(name, event) {
+      for (let i = 0; i < name.length; i++) {
+        message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+      }
+      return(message)
+  }
